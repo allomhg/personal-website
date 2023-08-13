9dce659e4087fb69141ebe68f929eacb74d7d2b9
allomhg <allomhg@gmail.com>
2023-08-13 12:09:20 +0100

    Updates

---
+++ src/pages/Projects.tsx
@@ -0,0 +1,16 @@
+import { FooterBar } from "../components/FooterBar";
+import { Greeting } from "../components/Greeting";
+import { Navbar } from "../components/Navbar";
+
+/* 
+    CODE SHOULDN'T BE REPEATED
+    NEED TO FIND A WAY TO PASS COMPONENTS TO A NEW PAGE?
+*/
+
+export const Projects = () => {
+    return (
+        <>
+            <h1>TEST</h1>
+        </>
+    );
+}
