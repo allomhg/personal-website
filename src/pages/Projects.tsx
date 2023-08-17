e5977f0f49866999b603e317cb732a5f5d47a530
allomhg <allomhg@gmail.com>
2023-08-17 20:45:06 +0100

    Minor updates

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -2,11 +2,24 @@ import Container from "@mui/material/Container";
 import Box from "@mui/material/Box";
 import Grid from '@mui/material/Unstable_Grid2'; // Grid version 2
 import { ProjectCard } from "../components/ProjectCard";
+import { Typography } from "@mui/material";
 
 export const Projects = () => {
     return (   
         <Box sx={{ display: "flex", flexGrow: 1, m: "auto", height: "100vh" }}>
-            <Grid container spacing={2} columns={12} sx={{ m: "auto"}}>
+            <Box>
+                <Typography variant="h5">
+
+                </Typography>
+            </Box>
+            
+            <Grid container spacing={6} columns={12} sx={{ m: "auto"}}>
+                <Grid xs={12} md={6} >
+                    <ProjectCard/>
+                </Grid>
+                <Grid xs={12} md={6} >
+                    <ProjectCard/>
+                </Grid>
                 <Grid xs={12} md={6} >
                     <ProjectCard/>
                 </Grid>
