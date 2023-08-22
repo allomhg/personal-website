e405ee0896e535af920067a9cf10ca7b46d745e6
allomhg <allomhg@gmail.com>
2023-08-22 21:46:47 +0100

    Working...

--- src/components/AboutContent.tsx
+++ src/components/AboutContent.tsx
@@ -2,11 +2,20 @@ import * as React from 'react';
 import Box from "@mui/material/Box";
 import Typography from '@mui/material/Typography';
 
+const experience = [
+    {startDate: "12-2017", endDate: "06-2022", jobTitle: "Design Engineer", place: "Patchell Industries Ltd."},
+]
+
 export const AboutContent = () => {
     return (
         <>
             <Box>
-
+                <Typography color="text.primary" variant='h2' component="h2">
+                    Experience
+                </Typography>
+                <Typography>
+                    
+                </Typography>
             </Box>
         </>
 
