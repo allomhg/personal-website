1007c02d5b3b79afe744c64f32e1f00c99b590d1
allomhg <allomhg@gmail.com>
2023-08-26 09:47:15 +0100

    Working...

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -4,25 +4,32 @@ import { AboutContent } from "../components/AboutContent";
 import { AboutImage } from "../components/AboutImage";
 
 
-interface AboutProps {
-    Job: {
-        startDate: string;
-        endDate: string;
-        jobTitle: string;
-        place: string;
-    }
-}
+// interface Job {
+//     key: string;
+//     endDate: string;
+//     jobTitle: string;
+//     place: string;
+// }
 
 // trying to find a way to pass information to my AboutContent component so I don't have to repeat code
 // I want to essentially be able to render X amount of AboutContent components depending on how many blocks of information I want
 // Such as certification, education, work experience, etc.
 // I need to pass information from these areas into the component using map (or something similar?)
 
-export const About = (props: AboutProps) => {
-    const experience: AboutProps = [
-        Job: {startDate: "12-2017", endDate: "06-2022", jobTitle: "Design Engineer", place: "Patchell Industries Ltd."},
+export const About = () => {
+    const experience = [
+        {date: "12-2022 - present", jobTitle: "Computer Aided Design Engineer", place: "Deepwater EU Ltd."},
+        {date: "12-2017 - 05-2022", jobTitle: "Mechanical Design Engineer", place: "Patchell Industries Ltd."},
+        {date: "11-2016 - 03-2017", jobTitle: "Design Assistant", place: "Patchell Industries Ltd."},
+        {date: "11-2015 - 03-2016", jobTitle: "Electronic Technician Assistant", place: "SI Lodec"},
     ]
-    
+
+    const certificates = [
+        {date: "08-2023", title: "", place: ""},
+        {date: "05-2023", title: "", place: ""},
+        {date: "02-2023", title: "", place: ""},
+    ]
+
     return (
         <Container maxWidth="lg" sx={{ 
                 display: "flex", 
@@ -31,6 +38,13 @@ export const About = (props: AboutProps) => {
             }}
         >
             <AboutImage/>
+            <Box>
+                {experience.map( (job) => (
+                    <AboutContent
+                    
+                    />
+                ))}
+            </Box>
             <AboutContent/>
         </Container>
     );
